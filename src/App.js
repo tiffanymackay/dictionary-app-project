536f2bd4b86d102c00e37dc74b0d353b506e12f1
tiffanymackay <88056784+tiffanymackay@users.noreply.github.com>
2021-09-25 16:46:36 -0700

    header update

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App container my-3 p-5">
-      <main>
+      <header>
         <img src={logo} className="App-logo img-fluid" alt="logo" />
 
-      </main>
+      </header>
     </div>
   );
 }
