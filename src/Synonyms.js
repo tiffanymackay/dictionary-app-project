f5f40c95cc418d207f215ca2cc53c5781a304ed2
tiffanymackay <88056784+tiffanymackay@users.noreply.github.com>
2021-09-26 10:22:23 -0700

    root color change

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,13 +1,14 @@
 import React from "react";
+import "./Synonyms.css"
 
 export default function Synonyms(props) {
     if (props.synonyms) {
         return (
-            <ul className="Synonyms">
+            <ul className="Synonyms list-inline">
                 {props.synonyms.map(function(synonym, index) {
                     return (
-                        <li key={index}>
-                            {synonym}
+                        <li className="list-inline-item" key={index}>
+                            {synonym},
                         </li>
                     )
                 })}
