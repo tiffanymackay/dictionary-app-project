5e06b2c8888a4a36b44476c162c72b103ddce01d
tiffanymackay <88056784+tiffanymackay@users.noreply.github.com>
2021-09-25 23:21:31 -0700

    meaning component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,11 +1,17 @@
 import React from "react";
+import "./Meaning.css";
 
 export default function Meaning(props) {
     console.log(props.meaning);
     return (<div className="Meaning">
         <h3>{props.meaning.partOfSpeech}</h3>
-        <p>{props.meaning.definitions[0].definition}</p>
-        <p>{props.meaning.definitions[0].example}</p>
-
+        {props.meaning.definitions.map(function (definition, index) {
+            return (
+                <div key={index}>
+                    <p>{definition.definition}</p>
+                    <p className="examples">{definition.example}</p>
+                </div>
+            );
+        })}        
     </div>);
 }
