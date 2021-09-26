3f62359fc8d9725874c4a4b0b964bb5207fb4b8a
tiffanymackay <88056784+tiffanymackay@users.noreply.github.com>
2021-09-25 18:07:19 -0700

    search bar

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,9 +2,25 @@ import React from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
+    function search(event) {
+        event.preventDefault();
+        alert("searching...");
+    }
+
     return (
-    <div className="container-box">
-        <h1>Hello</h1>
+    <div className="Dictionary container-box">  
+    <h1>Word lookup</h1>        
+        <form onSubmit={search}>
+            <div class="d-flex">
+                <div className="flex-grow-1">
+                    <input type="search" placeholder="Search" autoFocus={true} className="form-control" /> 
+                </div>
+                <div className="mx-1">
+                    <button value="Search" className="btn btn-secondary mx-1 px-3" type="submit">Search</button>
+                </div>
+            </div>
+        </form>
+  
     </div>
         );
 }
