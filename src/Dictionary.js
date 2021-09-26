b3fac7e61d5ec5ee35eeccb6bf569620a90f298c
tiffanymackay <88056784+tiffanymackay@users.noreply.github.com>
2021-09-25 18:16:58 -0700

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
+    let [keyword, setKeyword] = useState("");
+
     function search(event) {
         event.preventDefault();
-        alert("searching...");
+        alert(`Looking up: ${keyword}`);
+    }
+    function handleKeywordChange(event) {
+        setKeyword(event.target.value);
     }
 
     return (
     <div className="Dictionary container-box">  
     <h1>Word lookup</h1>        
         <form onSubmit={search}>
-            <div class="d-flex">
+            <div className="d-flex">
                 <div className="flex-grow-1">
-                    <input type="search" placeholder="Search" autoFocus={true} className="form-control" /> 
+                    <input onChange={handleKeywordChange} type="search" placeholder="Search" autoFocus={true} className="form-control" /> 
                 </div>
                 <div className="mx-1">
                     <button value="Search" className="btn btn-secondary mx-1 px-3" type="submit">Search</button>
